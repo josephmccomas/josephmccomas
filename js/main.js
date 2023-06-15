9c6566be541cf94d1b9f12eb39df0a9b0fdc0d43
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-06-15 17:07:42 -0500

    6-15-23 1707 Update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,3 @@
-for (let i of switches) {
-    i.addEventListener('click', function () {
-        let theme = this.dataset.theme;
-        console.log(theme);
-    });
-}
-
-function setTheme(theme) {
-    if (theme === 'light') {
-        document.getElementById('switcher-id').href = './styles/light.css';
-    } else if (theme === 'stars') {
-        document.getElementById('switcher-id').href = './styles/stars.css';
-    } else if (theme === 'dark') {
-        document.getElementById('switcher-id').href = './styles/dark.css';
-    }
-}
-
-for (let i of switches) {
-    i.addEventListener('click', function () {
-        let theme = this.dataset.theme;
-        console.log(theme);
-        setTheme(theme);
-    });
-}
-
-let style = localStorage.getItem('style');
-
-if (style == null) {
-    setTheme('light');
-} else {
-    setTheme(style);
-}
-
-localStorage.setItem('style', theme);
-
 let switches = document.getElementsByClassName('switch');
 
 let style = localStorage.getItem('style');
@@ -60,3 +25,69 @@ function setTheme(theme) {
     }
     localStorage.setItem('style', theme);
 }
+
+
+// for (let i of switches) {
+//     i.addEventListener('click', function () {
+//         let theme = this.dataset.theme;
+//         console.log(theme);
+//     });
+// }
+//
+// function setTheme(theme) {
+//     if (theme === 'light') {
+//         document.getElementById('switcher-id').href = './styles/light.css';
+//     } else if (theme === 'stars') {
+//         document.getElementById('switcher-id').href = './styles/stars.css';
+//     } else if (theme === 'dark') {
+//         document.getElementById('switcher-id').href = './styles/dark.css';
+//     }
+// }
+//
+// let style = localStorage.getItem('style');
+//
+// if (style == null) {
+//     setTheme('light');
+// } else {
+//     setTheme(style);
+// }
+//
+// localStorage.setItem('style', theme);
+//
+// let switches = document.getElementsByClassName('switch');
+//
+//
+//
+// // for (let i of switches) {
+// //     i.addEventListener('click', function () {
+// //         let theme = this.dataset.theme;
+// //         console.log(theme);
+// //         setTheme(theme);
+// //     });
+// // }
+//
+// // let style = localStorage.getItem('style');
+// //
+// // if (style == null) {
+// //     setTheme('light');
+// // } else {
+// //     setTheme(style);
+// // }
+//
+// // for (let i of switches) {
+// //     i.addEventListener('click', function () {
+// //         let theme = this.dataset.theme;
+// //         setTheme(theme);
+// //     });
+// // }
+//
+// // function setTheme(theme) {
+// //     if (theme === 'light') {
+// //         document.getElementById('switcher-id').href = './themes/light.css';
+// //     } else if (theme === 'stars') {
+// //         document.getElementById('switcher-id').href = './themes/stars.css';
+// //     } else if (theme === 'dark') {
+// //         document.getElementById('switcher-id').href = './themes/dark.css';
+// //     }
+// //     localStorage.setItem('style', theme);
+// // }
